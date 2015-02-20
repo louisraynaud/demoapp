8382f1c3d70ede978154b2e3362cbb84677384d4
louis raynaud <louis_raynaud@mba.berkeley.edu>
2015-02-20 04:13:51 +0000

    Changed home page path

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
 
-  get 'static_pages/help'
-
-  get 'static/Pages'
-
-  get 'static/home'
-
-  get 'static/help'
+  match 'help', to: 'static_pages#help', via: 'get'
 
   resources :users
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "h1", "DemoApp"
+    assert_select "h1", "DemoAgpp"
   end
 
   test "should get help" do
